41bf9921c174c97230ed03449efde26a2222d7f6
Faezehoraei <92444156+Faezehoraei@users.noreply.github.com>
2021-10-20 12:20:58 +0330

    Add files via upload

---
+++ Armstrong.py
@@ -0,0 +1,11 @@
+number = int(input("enter a number: "))
+sum = 0
+temp = number
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+if number == sum:
+   print(number,'yes')
+else:
+   print(number,'no')
